68dd2c8ccce11ac88671b5bc80c3caab1750deb2
Roman Puzanov <puzanovrs@gmail.com>
2021-03-18 22:21:00 +0500

    Пишу эвент при переходе к регистрации

--- docs/static/js/site.js
+++ docs/static/js/site.js
@@ -1,1 +1,6 @@
-﻿
+﻿function goToRegistration() {
+    ym(54628381,'reachGoal','go-to-registration');
+    window.setTimeout(function () {
+        location.href = "https://lk.deski.ru/#/account/create/";
+    }, 300);
+}
